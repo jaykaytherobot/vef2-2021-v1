7a6438dd1bd385ae7a40063f8087b854cd5e9564
jaykaytherobot <johannekarisolmundarson@gmail.com>
2021-01-20 11:20:21 +0000

    added minor changes

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 const path = require('path');
 const express = require('express');
+const videos = require('./videos');
 
 let app = express();
 
 // leyfa express að bera static skjöl
 app.use(express.static(path.join(__dirname, 'public')));
 
+// lætur express vita hvar ejs template eru og að við
+// erum að nota ejs engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//request handler fyrir heimasíðu
-app.get('/', (req, res) => {
-  let title = 'Vídeoleigan';
-  res.render('videos', {title});
-});
+app.use('/', videos);
+// //request handler fyrir heimasíðu
+// app.get('/', (req, res) => {
+//   let title = 'Vídeoleigan';
+//   res.render('videos', {title});
+// });
 
-// request handler fyrir önnur URL en heimasíðu
-app.get('/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.send(slug);
-});
+// // request handler fyrir önnur URL en heimasíðu
+// app.get('/:slug', (req, res) => {
+//   const { slug } = req.params;
+//   res.send(slug);
+// });
 
 const hostname = '127.0.0.1';
 const port = 3000;
